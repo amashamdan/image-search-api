6a5090ba994b27e142035faf064a62cf1fa89960
Amer Hamdan <amashamdan@gmail.com>
2016-10-06 12:00:27 -0700

    Feat: Added /search/:url route.
    The user can now enter a search string and search for a certain topic.
    The response sent to the user at this point is not manipulated, it is the whole response body.

--- server.js
+++ server.js
@@ -2,16 +2,23 @@ var express = require("express");
 var request = require("request");
 var app = express();
 
-var url = "https://www.googleapis.com/customsearch/v1?q=soccer&cx=003304933768532957899:opx7yx62ybk&key=AIzaSyDWuYmVM6ZgUWtdg_rIJC1pDcEx4Uldlrg"
-
 app.get("/", function(req, res) {
-	request(url, function(err, response, body) {
+	res.send("Enter search query in the address bar.");
+});
+
+app.get("/search/:url", function(req, res) {
+	console.log(req.params.url);
+	var searchUrl = "https://www.googleapis.com/customsearch/v1?q=" + req.params.url + "&cx=003304933768532957899:opx7yx62ybk&key=AIzaSyDWuYmVM6ZgUWtdg_rIJC1pDcEx4Uldlrg"
+	request(searchUrl, function(err, response, body) {
 		console.log(typeof(body), typeof(response));
 		res.send(JSON.parse(body));
 		res.end();
 	});
-});
+})
 
 // port 3000 used for localhost during development.
 var port = Number(process.env.PORT || 3000)
 app.listen(port);
+
+/* add index.html for root
+add time of search */
