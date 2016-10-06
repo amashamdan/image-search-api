1b94ddb7a3a646da42e9e0ece4cae4983cfc7238
Amer Hamdan <amashamdan@gmail.com>
2016-10-06 13:44:38 -0700

    Feat: Added createResponse function to display search results.
    The user can now enter a search string and receive results.
    The function createResponse exctracts the data needed for the response and create the response array.
    The function is called from the get mothod callback function.
    Next, enable the user to add an offet.

--- server.js
+++ server.js
@@ -7,14 +7,35 @@ app.get("/", function(req, res) {
 });
 
 app.get("/search/:url", function(req, res) {
-	console.log(req.params.url);
 	var searchUrl = "https://www.googleapis.com/customsearch/v1?q=" + req.params.url + "&cx=003304933768532957899:opx7yx62ybk&key=AIzaSyDWuYmVM6ZgUWtdg_rIJC1pDcEx4Uldlrg"
 	request(searchUrl, function(err, response, body) {
-		console.log(typeof(body), typeof(response));
-		res.send(JSON.parse(body));
-		res.end();
+		var responseBody = JSON.parse(body);
+		var returnedBody = [];
+		createResponse(res, responseBody, returnedBody);
 	});
 })
+
+function createResponse(res, responseBody, returnedBody) {
+	for (var item in responseBody.items) {
+		var resObject = {};
+		if (responseBody.items[item].pagemap.cse_image) {
+			resObject.imageurl = responseBody.items[item].pagemap.cse_image[0].src;
+		} else {
+			resObject.imageurl = "Cannot be read";
+		}
+		if (responseBody.items[item].pagemap.cse_thumbnail) {
+			resObject.thumbnail = responseBody.items[item].pagemap.cse_thumbnail[0].src;
+		} else {
+			resObject.thumbnail = "Cannot be read";
+		}
+		resObject.pageUrl = responseBody.items[item].link;
+		resObject.snippet = responseBody.items[item].title;
+		returnedBody.push(resObject);
+	}
+	res.send(returnedBody);
+	//res.send(responseBody);
+	res.end();
+}
 
 // port 3000 used for localhost during development.
 var port = Number(process.env.PORT || 3000)
